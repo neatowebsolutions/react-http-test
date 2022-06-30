f5846b473c29a4cc16ed9af66b55806927b1c572
kirkwood616 <kirkwood616@gmail.com>
2022-06-30 17:47:04 -0400

    Adding changes

--- src/hooks/test.js
+++ src/hooks/test.js
@@ -1,26 +1,41 @@
-import useSWR from 'swr';
-import HttpClient from '../httpClient';
+import axios from "axios";
+import useSWR from "swr";
 
-const httpClient = new HttpClient({
-  baseUrl: 'http://localhost:3002'
-});
+// const httpClient = new HttpClient({
+//   baseUrl: 'http://localhost:3002'
+// });
 
 // const fetcher = (url) => fetch(url).then((r) => r.json());
 // const fetcher = (url) =>
 //   client({ method: 'GET', url }).then((response) => response.data);
 
-const useSkills = () => {
-  const { data, error } = useSWR(
-    `/categories`,
-    httpClient.get.bind(httpClient),
-    {
-      revalidateOnFocus: false
-    }
-  );
+// const useSkills = () => {
+//   const { data, error } = useSWR(
+//     `/categories`,
+//     httpClient.get.bind(httpClient),
+//     {
+//       revalidateOnFocus: false
+//     }
+//   );
 
+const client = axios.create();
+
+client.defaults.baseURL = "http://localhost:3002";
+client.defaults.timeout = 20 * 1000;
+client.defaults.responseType = "json";
+client.defaults.headers.common["Content-Type"] = "application/json; charset=utf-8";
+
+function fetcher(url) {
+  client.get(url).then((res) => res.data);
+}
+
+const useSkills = () => {
+  const { data, error } = useSWR("/categories", fetcher, {
+    revalidateOnFocus: false,
+  });
   return {
     data,
-    error
+    error,
   };
 };
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,10 @@
-import Head from 'next/head';
-import useTest from '../hooks/test';
+import axios from "axios";
+import Head from "next/head";
+import useTest from "../hooks/test";
 
 const HomePage = () => {
   const { data, error } = useTest();
+  const data2 = axios.get("http://localhost:3002/categories").then((res) => res.data);
 
   return (
     <>
