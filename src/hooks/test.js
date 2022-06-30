a5cd531ac0ad157cfac10d997a613f95d98a375a
Chad Johnson <chad.d.johnson@gmail.com>
2022-06-30 14:11:10 -0400

    Using axios

--- src/hooks/test.js
+++ src/hooks/test.js
@@ -1,9 +1,20 @@
+import axios from 'axios';
 import useSWR from 'swr';
 
-const fetcher = (url) => fetch(url).then((r) => r.json());
+const client = axios.create();
+
+client.defaults.baseURL = 'http://localhost:3002';
+client.defaults.timeout = 20 * 1000;
+client.defaults.responseType = 'json';
+client.defaults.headers.common['Content-Type'] =
+  'application/json; charset=utf-8';
+
+// const fetcher = (url) => fetch(url).then((r) => r.json());
+const fetcher = (url) =>
+  client({ method: 'GET', url }).then((response) => response.data);
 
 const useSkills = () => {
-  const { data, error } = useSWR(`/api/test`, fetcher, {
+  const { data, error } = useSWR(`/categories`, fetcher, {
     revalidateOnFocus: false
   });
 
